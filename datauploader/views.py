8ee43b03fa3d30311f8feb6050027da1568829bf
Syamcharan2406 <144098199+Syamcharan2406@users.noreply.github.com>
2024-10-25 22:10:39 +0530

    giving acess to admin portal for staff only and chaged styling for register page

--- datauploader/views.py
+++ datauploader/views.py
@@ -2,9 +2,9 @@ from django.shortcuts import render, HttpResponse, redirect, get_object_or_404
 from .forms import CourseForm, CourseConceptsForm
 from .models import CourseConcepts, Courses
 
-
-
-
+from django.contrib.auth.decorators import user_passes_test,login_required
+@login_required(login_url='myapp:login')
+@user_passes_test(lambda user: user.is_staff)
 def CourseUpload(request):
     f = CourseForm()
     if request.method == "POST":
@@ -18,7 +18,8 @@ def CourseUpload(request):
 
 
 
-
+@login_required(login_url='myapp:login')
+@user_passes_test(lambda user: user.is_staff)
 def conceptUpload(request):
     f = CourseConceptsForm()
     if request.method == "POST":
@@ -28,21 +29,21 @@ def conceptUpload(request):
             return redirect('datauploader:concept_list')
     return render(request, 'coursesAdmin/conceptUpload.html', {'form': f})
 
-
-
+@login_required(login_url='myapp:login')
+@user_passes_test(lambda user: user.is_staff)
 def getData(request):
     data = Courses.objects.all()
     return render(request, 'coursesAdmin/courses.html', {'data': data})
 
-
-
+@login_required(login_url='myapp:login')
+@user_passes_test(lambda user: user.is_staff)
 def delete_course(request, id):
     course = get_object_or_404(Courses, id=id)
     course.delete()
     return redirect('datauploader:course_list')
 
-
-
+@login_required(login_url='myapp:login')
+@user_passes_test(lambda user: user.is_staff)
 def update_course(request, id):
     course = get_object_or_404(Courses, id=id)
     if request.method == "POST":
@@ -54,14 +55,14 @@ def update_course(request, id):
         form = CourseForm(instance=course)
     return render(request, 'coursesAdmin/courseUpload.html', {'form': form})
 
-
-
+@login_required(login_url='myapp:login')
+@user_passes_test(lambda user: user.is_staff)
 def concept_list(request):
     concepts = CourseConcepts.objects.all()
     return render(request, 'coursesAdmin/concept_list.html', {'concepts': concepts})
 
-
-
+@login_required(login_url='myapp:login')
+@user_passes_test(lambda user: user.is_staff)
 def delete_concept(request, id):
     concept = get_object_or_404(CourseConcepts, id=id)
     if request.method == "POST":
